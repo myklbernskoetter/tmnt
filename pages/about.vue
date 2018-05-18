2eaacc59f4c4fc3a71af968779917969df5f717a
Mykl Bernskoetter <mbernskoetter@barkleyus.com>
2018-05-18 11:08:36 -0500

    removing about page image

--- pages/about.vue
+++ pages/about.vue
@@ -35,7 +35,7 @@
     </div>
     <div class="second-column page-detail-wrapper">
       <div class="learning-blurb">
-        <div class="about-image-wrapper"><img class="about-image" src="~/assets/images/about-image1.jpg" alt=""></div>
+        <!-- <div class="about-image-wrapper"><img class="about-image" src="~/assets/images/about-image1.jpg" alt=""></div> -->
         <h2>What am I learning about?</h2>
         <p>
           In this profession, it's not okay to just rest on your laurels.
