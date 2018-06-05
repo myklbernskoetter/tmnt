d0008502ed98ea60c6b7b8c9acfb5f8b5d513ce6
MYKL <mbernskoetter5@gmail.com>
2018-06-05 14:59:11 -0500

    resume file update kollectif font attribution removed

--- pages/contact.vue
+++ pages/contact.vue
@@ -21,7 +21,6 @@
     </div>
     <footer class="site-footer">
       <div class="attributions">
-        kollektif font by <a href="http://www.bravenewfonts.com/">Brave New Fonts</a>
         <div>Icons made by:
           <span class="icon-attribution"><a rel="noopener" target="_blank" href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></span>
           <span class="icon-attribution"><a rel="noopener" target="_blank" href="https://www.flaticon.com/authors/uiuxer" title="UIUXER">UIUXER</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></span>
